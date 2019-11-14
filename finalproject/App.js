44d4831d590fc47c9269db23447791af78feff32
lki9419 <kl8jc@virginia.edu>
2019-11-14 14:28:48 -0500

    Bottom Tab for Pages
    Changed so there are bottom tabs for navigating to three pages

--- finalproject/App.js
+++ finalproject/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 // You can import from local files
 import Upload from './components/UserScreen';
@@ -10,17 +10,26 @@ import Upload from './components/UserScreen';
 import { Card } from 'react-native-paper';
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'MemeDer',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Swiping Screen</Text>
+      </View>
+    );
+  }
 
+}
+
+class LikedScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Liked Screen</Text>
+      </View>
+    );
+  }
+}
+class UserScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -42,84 +51,29 @@ class HomeScreen extends React.Component {
             </Text>
           </View>
         </View>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            });
-          }}
-        />
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Details',
-  };
-
-  render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen,
-  },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
+const TabNavigator = createBottomTabNavigator({
+  User: { screen: UserScreen},
+  Home: { screen: HomeScreen },
+  Liked: { screen: LikedScreen },
+  },{
+    tabBarOptions: {
+      activeTintColor: 'black',
+      labelStyle: {
+        fontSize: 20,
       },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
+      style: {
+        backgroundColor: 'skyblue',
       },
-    },
+    }
   }
 );
 
-const AppContainer = createAppContainer(RootStack);
-
-export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
-}
+export default createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
